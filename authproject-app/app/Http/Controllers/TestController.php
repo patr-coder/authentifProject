693474ea0a03d93369bb54f43c539cc902b5fe87
patrick_muk <patrickmukadi2017@gamil.com>
2022-01-27 00:36:00 +0900

    create start authproject-app

---
+++ authproject-app/app/Http/Controllers/TestController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
+
+class TestController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth')->except('bar');
+    }
+
+
+    public function foo()
+    {
+        if(!Gate::allows('access-admin')){
+            abort('403');
+        }
+       return view('test.foo');
+    }
+    
+
+    public function bar()
+    {
+        return view('test.bar');
+    }
+}
